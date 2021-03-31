b910beeaa8f3a939f2f7bbfcb6a58bc821222c11
armanziaei <armanziaei83@gmaio.com>
2021-03-31 12:32:43 +0430

    Feat Builder Class : builder class implemented to Engineer

---
+++ app/src/main/java/com/example/daggerruntimevalues/house/Builder.java
@@ -0,0 +1,22 @@
+package com.example.daggerruntimevalues.house;
+
+public class Builder implements Engineer {
+
+    String name;
+    int age;
+
+    @Override
+    public void setEngineerName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setEngineerAge(int age) {
+        this.age = age;
+    }
+    
+    public Builder(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
